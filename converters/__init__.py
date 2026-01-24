d4283a6a5e89a86a9e67788c90739bf7dff92c0b
cebem1nt <mineewarik@gmail.com>
2026-01-24 18:30:02 -0300

    Fix attributes of converters, allow audio

--- converters/__init__.py
+++ converters/__init__.py
@@ -1,5 +1,5 @@
-from converters.images import convert_image
 from .images import convert_image
 from .tables import csv_to_xlsx, xlsx_to_csv
+from .audio import audio_convert
 
-__all__ = ["convert_image", "csv_to_xlsx", "xlsx_to_csv"]
+__all__ = ["convert_image", "csv_to_xlsx", "xlsx_to_csv", "audio_convert"]
