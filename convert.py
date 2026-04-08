93ab159305f7f904bd93361e9f8055052c499ced
cebem1nt <mineewarik@gmail.com>
2026-04-08 15:57:47 -0300

    Added handling directories

--- convert.py
+++ convert.py
@@ -12,37 +12,46 @@ def get_file_name(f: str):
     file_name, _ =  tail.rsplit('.', 1)
     return file_name
 
+def convert(src: str, target: str, dest: str | None):
+    if not os.path.exists(src):
+        sys.stderr.write(f'File "{src}" does not exist\n')
+        exit(1)
+
+    target_path = get_file_name(src)
+    target_path += '.' + target
+
+    if dest:
+        target_path = os.path.join(dest, target_path)
+
+    for Converter in converters:
+        c = Converter(src, target_path)
+
+        try:
+            if c.can_convert(): c.convert(); break
+
+        except KeyboardInterrupt:
+            sys.stderr.write("Conversion was interrupted!\n")
+            exit(1)
+    else:
+        print(f"Could not convert \"{src}\" to \".{target}\", unsupported extension(s)")
+
 def main(args: argparse.Namespace):
     args.target = args.target.lstrip('.') # strip optional .
 
     if args.dest:
         os.makedirs(args.dest, exist_ok=True)
 
-    for src in args.source:
-        print(f"Processing: {src}")
+    sources = args.sources
 
-        if not os.path.exists(src):
-            sys.stderr.write(f'File "{src}" does not exist\n')
-            return 1
+    for src in sources:
+        print(src)
 
-        target_path = get_file_name(src)
-        target_path += '.' + args.target
-
-        if args.dest:
-            target_path = os.path.join(args.dest, target_path)
-
-        for Converter in converters:
-            c = Converter(src, target_path)
-
-            try:
-                if c.can_convert(): c.convert(); break
-
-            except KeyboardInterrupt:
-                sys.stderr.write("Conversion was interrupted!\n")
-                return 1
-
-        else:
-            print(f"Could not convert {src} to {args.target}, unsupported extension(s)")
+        if os.path.isdir(src):
+            files = os.listdir(src)
+            sources += [os.path.join(src, f) for f in files]
+            continue
+    
+        convert(src, args.target, args.dest)
 
     return 0
 
@@ -52,9 +61,9 @@ if __name__ == "__main__":
         description="Convert files to different file types"
     )
     
-    p.add_argument("source",        type=str, nargs="*", help="Source file[s]")
-    p.add_argument("target",        type=str,            help="Target extension")
-    p.add_argument("-d", "--dest",  type=str,            help="Destination directory")
+    p.add_argument("sources",      type=str, nargs="*", help="Source file[s]")
+    p.add_argument("target",       type=str,            help="Target extension")
+    p.add_argument("-d", "--dest", type=str,            help="Destination directory")
 
     args = p.parse_args()
     
